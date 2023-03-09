84c0b8625099c9de992f14ab641b824c13237ebd
Zihao1319 <zihao1319@gmail.com>
2023-03-09 13:41:57 +0800

    first commit

---
+++ src/main/java/day21_paf/day21_lecture/Day21LectureApplication.java
@@ -0,0 +1,13 @@
+package day21_paf.day21_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day21LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day21LectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/day21_paf/day21_lecture/Day21LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package day21_paf.day21_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day21LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
